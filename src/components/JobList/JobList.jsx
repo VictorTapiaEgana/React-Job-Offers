aaec7c0ba45c2fb1000a46fb0bfca4f4d0be8af7
Victor <victortapiaegana@gmail.com>
2024-03-12 02:41:36 -0400

    ADD:filtro por rol

--- src/components/JobList/JobList.jsx
+++ src/components/JobList/JobList.jsx
@@ -2,13 +2,13 @@ import JobCard from "../JobCard/JobCard";
 import "./JobList.css"
 
 import joboffers from "../../../data.json";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import JobFilter from "../JobFilter/JobFilter";
 
 const JobList = () =>{
    
-
-    const [ arrayRole , setArrayRole ]  = useState();
+    const [ofertasFiltradas ,setOfertasFiltradas] = useState(joboffers)
+    const [ arrayRole , setArrayRole ]  = useState('Frontend');
     const [ arrayLevel , setArrayLevel ]  = useState();
     const [ arrayLenguajes , setArrayLenguajes ]  = useState([]);
     const [ arrayTools , setArrayTools ]  = useState([]);
@@ -22,6 +22,19 @@ const JobList = () =>{
         const existeElFiltro = arrayTools.includes(filtro);
         !existeElFiltro && setArrayTools([...arrayTools, filtro]);
     }
+    
+    useEffect(() => {
+        console.log('useEffect', ofertasFiltradas);
+        
+        let newFilter = ofertasFiltradas;
+
+        if (arrayRole) {
+            newFilter = newFilter.filter(filt => filt.role === arrayRole);
+        }
+
+        setOfertasFiltradas(newFilter);
+
+    }, [arrayRole]);
 
     return(
         <> 
@@ -38,30 +51,29 @@ const JobList = () =>{
                 />
             )}
 
-            {joboffers.map(jobs =>{
+            {ofertasFiltradas.map(jobs =>{
                 return(
-                  <JobCard 
-                          key={ jobs.id }
-                          company = { jobs.company } 
-                          logo = { jobs.logo }
-                          neww = { jobs.new }
-                          featured={ jobs.featured }
-                          position ={ jobs.position }
-                          role = { jobs.role }
-                          level = { jobs.level }
-                          postedAt = { jobs.postedAt }
-                          contract = { jobs.contract }
-                          location = { jobs.location }
-                          languages = { jobs.languages }
-                          tools = { jobs.tools }                          
-                          setArrayRole = { setArrayRole }
-                          setArrayLevel = { setArrayLevel }
-                          GenerarArrayLenguajes = {  GenerarArrayLenguajes }                          
-                          GenerarArrayTools = { GenerarArrayTools }
-                  />
+                    <JobCard 
+                            key={ jobs.id }
+                            company = { jobs.company } 
+                            logo = { jobs.logo }
+                            neww = { jobs.new }
+                            featured={ jobs.featured }
+                            position ={ jobs.position }
+                            role = { jobs.role }
+                            level = { jobs.level }
+                            postedAt = { jobs.postedAt }
+                            contract = { jobs.contract }
+                            location = { jobs.location }
+                            languages = { jobs.languages }
+                            tools = { jobs.tools }                          
+                            setArrayRole = { setArrayRole }
+                            setArrayLevel = { setArrayLevel }
+                            GenerarArrayLenguajes = {  GenerarArrayLenguajes }                          
+                            GenerarArrayTools = { GenerarArrayTools }
+                    />
                 )
-
-            })}            
+            })}
         </>
     )
 }
